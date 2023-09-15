1eaad0a2b36a3f9aeb6b8afe257199482fdc8e20
zzirnheld <zzirnheld@gmail.com>
2023-09-15 14:30:15 -0500

    Remember to change the name of the scene correctly

--- Scripts/UI/Main Menu/MainMenu.cs
+++ Scripts/UI/Main Menu/MainMenu.cs
@@ -6,7 +6,7 @@ namespace Kompas.UI.MainMenu
 	public partial class MainMenu : Control
 	{
 		private const string ClientScenePath = "res://Scenes/ClientScene.tscn";
-		private const string BuildDeckPath = "res://Scenes/BuildDeck.tscn";
+		private const string BuildDeckPath = "res://Scenes/BuildDeckScene.tscn";
 
 		private void ConnectToServer() => GetTree().ChangeSceneToFile(ClientScenePath);
 		private void BuildDeck() => GetTree().ChangeSceneToFile(BuildDeckPath);
