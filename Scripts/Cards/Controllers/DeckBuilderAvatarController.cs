93d5b626e00a82fb1672e4d3a51837a1881a4f7a
zzirnheld <zzirnheld@gmail.com>
2023-09-18 17:23:17 -0500

    Don't show the old avatar when player hovers over a blank avatar spot

--- Scripts/Cards/Controllers/DeckBuilderAvatarController.cs
+++ Scripts/Cards/Controllers/DeckBuilderAvatarController.cs
@@ -1,12 +1,14 @@
-using Godot;
-using Kompas.Cards.Controllers;
 using Kompas.Cards.Models;
 
 namespace Kompas.Cards.Controllers
 {
 	public partial class DeckBuilderAvatarController : DeckBuilderDeckCardController
 	{
-		public void Clear() => InfoDisplayer.Clear();
+		public void Clear()
+		{
+			InfoDisplayer.Clear();
+			Card = null;
+		}
 
 		public void UpdateAvatar(DeckBuilderCard card) => Card = card;
 	}
