3d0354410e6b99ae2008f5d035469a46ff08d731
zzirnheld <zzirnheld@gmail.com>
2023-10-01 13:55:46 -0500

    Add padding

--- Scripts/UI/Deck Builder/BuiltDeckContainer.cs
+++ Scripts/UI/Deck Builder/BuiltDeckContainer.cs
@@ -2,12 +2,33 @@ using Godot;
 
 namespace Kompas.UI.DeckBuilder
 {
+	//IMPL NOTE: I can't override the AddChild methods 1) because they take in nodes, not controls and 2) because they're not virtual
 	public partial class BuiltDeckContainer : Control
 	{
+		private const int PositionalLayoutMode = 0;
+
+		[Export]
+		private int ColumnCount { get; set; } = 8;
+
+		[Export]
+		private float Padding { get; set; } = 5f;
+
 		public override void _Ready()
 		{
 			base._Ready();
 			Resized += Resize;
+		}
+
+		public void AddChild(Control child)
+		{
+			base.AddChild(child);
+			ResizeChild(child);
+		}
+
+		public void MoveChild(Control childNode, int toIndex)
+		{
+			base.MoveChild(childNode, toIndex);
+			Resize();
 		}
 
 		private void Resize()
@@ -18,27 +39,17 @@ namespace Kompas.UI.DeckBuilder
 			}
 		}
 
-		public void AddChild(Control child)
-		{
-			base.AddChild(child);
-			ResizeChild(child);
-		}
-
 		private void ResizeChild(Control child)
 		{
-			child.LayoutMode = 0;
-			child.Size = new Vector2(Size.X / 8f, Size.X / 8f);
+			child.LayoutMode = PositionalLayoutMode;
+			//When determining offset, we add padding to the size to account for the padding we're adding after the last element
+			float offset = (Size.X + Padding) / ColumnCount;
+			float tileSize = offset - Padding;
+			child.Size = new Vector2(tileSize, tileSize);
 
-			float column = child.GetIndex() % 8;
-			float row = (float) (child.GetIndex() / 8);
-			child.Position = new(child.Size.X * column, child.Size.Y * row); //todo padding?
-			GD.Print($"{Size / 8f}, {child.Size}, {child.Size.Y * row} for row {row}, {child.Position}");
-		}
-
-		public void MoveChild(Control childNode, int toIndex)
-		{
-			base.MoveChild(childNode, toIndex);
-			Resize();
+			float column = child.GetIndex() % ColumnCount;
+			float row = child.GetIndex() / ColumnCount;
+			child.Position = new(offset * column, offset * row); //todo padding?
 		}
 	}
 }
