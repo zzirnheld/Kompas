38aa5ef0b17a23ab77030987323d0e22fe63ff5b
zzirnheld <zzirnheld@gmail.com>
2024-04-26 19:16:36 -0500

    Fix modular golem cores, leave placeholder of what I'll need to do one day

--- Scripts/Shared/JsonHelper.cs
+++ Scripts/Shared/JsonHelper.cs
@@ -1,20 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Reflection;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace Kompas.Shared
 {
 	public static class JsonHelper
 	{
-		public static string PrettifyJson(string json)
+        /*
+        public static readonly ISerializationBinder KompasTypesBinder = ;
+
+        private class KompasTypesBinder : ISerializationBinder
         {
-            using (var stringReader = new StringReader(json))
-            using (var stringWriter = new StringWriter())
+            private ICollection<Type> allowedTypes = Assembly.GetExecutingAssembly().GetTypes()
+                .Where(type => type.Namespace.Contains("Kompas"))
+                .ToArray();
+
+            public void BindToName(Type serializedType, out string? assemblyName, out string? typeName)
             {
-                var jsonReader = new JsonTextReader(stringReader);
-                var jsonWriter = new JsonTextWriter(stringWriter) { Formatting = Formatting.Indented };
-                jsonWriter.WriteToken(jsonReader);
-                return stringWriter.ToString();
+                assemblyName = null;
+                typeName = serializedType.Name;
             }
+
+            public Type BindToType(string? assemblyName, string typeName)
+            {
+                return allowedTypes.Single(t => t.Name == typeName);
+            }
+        }*/ //TODO
+
+        public static string PrettifyJson(string json)
+        {
+            using var stringReader = new StringReader(json);
+            using var stringWriter = new StringWriter();
+
+            var jsonReader = new JsonTextReader(stringReader);
+            var jsonWriter = new JsonTextWriter(stringWriter) { Formatting = Formatting.Indented };
+
+            jsonWriter.WriteToken(jsonReader);
+            return stringWriter.ToString();
         }
 	}
 }
