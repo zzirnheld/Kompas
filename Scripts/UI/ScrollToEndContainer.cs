fee9a5e13efa592a74f968e93edfa305f2f1436f
zzirnheld <zzirnheld@gmail.com>
2024-05-05 13:51:08 -0500

    Scroll to end container

---
+++ Scripts/UI/ScrollToEndContainer.cs
@@ -0,0 +1,17 @@
+using Godot;
+
+namespace Kompas.UI
+{
+	public partial class ScrollToEndContainer : ScrollContainer
+	{
+		public override void _Ready()
+		{
+			base._Ready();
+			var scrollBar = GetVScrollBar();
+			scrollBar.Changed += () =>
+			{
+				if (scrollBar.MaxValue != scrollBar.Value) scrollBar.Value = scrollBar.MaxValue;
+			};
+		}
+	}
+}
