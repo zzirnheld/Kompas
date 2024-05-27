8a5b98985144e11484c19b688733975ea4f1f9da
zzirnheld <zzirnheld@gmail.com>
2024-05-27 15:55:16 -0500

    Move classes to their own files

---
+++ Scripts/Effects/Models/EventContext/EventCapturer.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kompas.Effects.Models.TriggeringEvent
+{
+    public class EventCapturer
+	{
+		private readonly IReadOnlyCollection<IIncompleteEventContext> incompletes;
+
+		public EventCapturer(IEnumerable<IIncompleteEventContext> incompletes)
+		{
+			this.incompletes = incompletes.ToArray();
+		}
+
+		public delegate void CapturableEvent();
+
+		public static IReadOnlyCollection<IEventContext> Capture(IEnumerable<IIncompleteEventContext> incompletes, CapturableEvent capturableEvent)
+		{
+			var capturer = new EventCapturer(incompletes);
+			return capturer.Capture(capturableEvent);
+		}
+
+		public static IReadOnlyCollection<IEventContext> Capture(CapturableEvent capturableEvent, params IIncompleteEventContext[] incompletes)
+			=> Capture(incompletes, capturableEvent);
+
+		public IReadOnlyCollection<IEventContext> Capture(CapturableEvent capturableEvent)
+		{
+			capturableEvent();
+			return incompletes
+				.Select(incomplete => incomplete.CacheAfterEvent())
+				.ToArray();
+		}
+	}
+}
