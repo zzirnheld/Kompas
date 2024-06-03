84713070cace1192e8260f10ca561d8dd3495668
zzirnheld <zzirnheld@gmail.com>
2024-06-03 15:50:59 -0500

    Make the augments start the spread at an offset angle, and not clip into the card in annoying ways

--- Scripts/Cards/Controllers/CardAugmentsController.cs
+++ Scripts/Cards/Controllers/CardAugmentsController.cs
@@ -6,18 +6,23 @@ using System.Linq;
 
 namespace Kompas.Cards.Controllers
 {
+	//FUTURE: if I don't like that the augments are moved around with the currently selected card,
+	//will need to move the augment controller out of the card model controller
+	//very doable, fortunately
 	public partial class CardAugmentsController : Node
 	{
-		private const float VerticalStackIncrement = 0.05f;
+		private const float VerticalStackBaseHeight = 0.05f;
+		private const float VerticalStackIncrement = 0.1f;
 		private const float VerticalSpreadHeight = 0.25f;
 		
-		private const float HorizontalStackIncrement = 0.05f;
+		private const float HorizontalStackIncrement = 0.1f;
 		private const float HorizontalSpreadRadius = 1f;
 
 		private const float AugmentScaleInStack = 0.75f;
 		private const float AugmentScaleInSpread = AugmentScaleInStack;
 
-		private static readonly Vector3 Rotation = new(0, Mathf.Pi, 0);
+		private const float SpreadAngleStartOffset = Mathf.Pi / 4f;
+		private static readonly Vector3 TurnAroundRotation = new(0, Mathf.Pi, 0);
 
 		/// <summary>
 		/// IMPL NOTE: Does not remove/free children.
@@ -34,10 +39,10 @@ namespace Kompas.Cards.Controllers
 				node.Visible = true;
 				node.Scale = Vector3.One * AugmentScaleInStack;
 
-				node.Rotation = card.Card.ControllingPlayer.Index * Rotation;
+				node.Rotation = card.Card.ControllingPlayer.Index * TurnAroundRotation;
 
-				node.Position = (Vector3.Up * index * VerticalStackIncrement)
-							  + (Vector3.Right * index * HorizontalStackIncrement); //TODO better spread
+				node.Position = (Vector3.Up * ((index * VerticalStackIncrement) + VerticalStackBaseHeight))
+							  + (Vector3.Right * index * HorizontalStackIncrement);
 			}
 		}
 
@@ -55,10 +60,10 @@ namespace Kompas.Cards.Controllers
 				node.Visible = true;
 				node.Scale = Vector3.One * AugmentScaleInSpread;
 
-				node.Rotation = card.Card.ControllingPlayer.Index * Rotation;
+				node.Rotation = card.Card.ControllingPlayer.Index * TurnAroundRotation;
 
 				float proportion = (float)index / count;
-				var spreadAngle = 2 * Mathf.Pi * proportion;
+				var spreadAngle = (2 * Mathf.Pi * proportion) + SpreadAngleStartOffset;
 				node.Position = (Vector3.Up * VerticalSpreadHeight)
 							  + (Vector3.Right * HorizontalSpreadRadius * Mathf.Cos(spreadAngle))
 							  + (Vector3.Back * HorizontalSpreadRadius * Mathf.Sin(spreadAngle));
