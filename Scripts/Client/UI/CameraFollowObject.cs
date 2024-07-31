90c17ead3693b0cb4ba05bec9560ccebfc843fd4
zzirnheld <zzirnheld@gmail.com>
2024-07-31 15:52:20 -0500

    Some notes about future work

--- Scripts/Client/UI/CameraFollowObject.cs
+++ Scripts/Client/UI/CameraFollowObject.cs
@@ -30,6 +30,15 @@ public partial class CameraFollowObject : Camera3D
 	{
 		GD.Print($"Following {follow} with mask {layerMask}");
 		if (node != follow) lerp = 0;
+		else lerp = 1f;
+
+		//TODO: want the motion to appear continuous as it changes the focusing of the card, so... maybe do some offsetting of the origin position/basis?
+		//Will need to do math so that the offset accounts for the current rotation...
+		//Once I figure out that math, I can remove the else case above to have motion appear to "finish"
+
+		//Might also/instead want to make it a cubic interpolation to maybe ease off on the stutteriness if you go from one card to another in a grid while having a card highlighted.
+		//still, really good spot for now.
+
 		follow = node;
 		CullMask = layerMask;
 		originPosition = GlobalPosition;
