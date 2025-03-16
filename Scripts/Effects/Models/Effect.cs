5295b4507a726f9b368d640a44c920a3edd458a6
Zacharie Zirnheld <zzirnheld@gmail.com>
2025-03-16 15:45:31 -0500

    Move IEffect to new file

--- Scripts/Effects/Models/Effect.cs
+++ Scripts/Effects/Models/Effect.cs
@@ -2,50 +2,11 @@ using System.Collections.Generic;
 using Kompas.Cards.Models;
 using Kompas.Effects.Models.Identities;
 using Kompas.Effects.Models.Restrictions;
-using Kompas.Effects.Models.TriggeringEvent;
 using Kompas.Effects.Subeffects;
 using Kompas.Gamestate;
-using Kompas.Gamestate.Exceptions;
 using Kompas.Gamestate.Players;
 
 namespace Kompas.Effects.Models;
-
-public interface IEffect : IStackable
-{
-	public IGame Game { get; }
-
-	public ITriggerRestriction? TriggerRestriction { get; }
-	public IActivationRestriction? ActivationRestriction { get; }
-
-	public int TimesUsedThisTurn { get; }
-	public int TimesUsedThisRound { get; }
-	public int TimesUsedThisStack { get; set; }
-
-	public event System.EventHandler<IEffect>? EffectInformationChanged;
-
-	public bool Negated { get; set; }
-
-	public int EffectIndex { get; }
-
-	public Trigger? Trigger { get; }
-
-	public void Reset();
-}
-
-public static class EffectExtensions
-{
-	public static int? MaxPerTurn(this IEffect effect)
-		=> effect.TriggerRestriction?.MaxUsesPerTurn
-		?? effect.ActivationRestriction?.MaxUsesPerTurn;
-
-	public static int? MaxPerRound(this IEffect effect)
-		=> effect.TriggerRestriction?.MaxUsesPerRound
-		?? effect.ActivationRestriction?.MaxUsesPerRound;
-
-	public static int? MaxPerStack(this IEffect effect)
-		=> effect.TriggerRestriction?.MaxUsesPerStack
-		?? effect.ActivationRestriction?.MaxUsesPerStack;
-}
 
 /// <summary>
 /// Effects will only be resolved on server. Clients will just get to know what effects they can use
